abde7f5032550ec839b20f47b0c74cdb8d03e591
KN <knny.nguyen@gmail.com>
2018-12-06 19:31:36 -0500

    comp will mount {queryString}

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import queryString from "query-string";
 
 class App extends Component {
+
+  componentWillMount() {
+    var query = queryString.parse(this.props.location.search);
+    if (query.token) {
+      window.localStorage.setItem("jwt", query.token);
+      this.props.history.push("/");
+   }
+}
+
   render() {
     return (
       <div className="App">
